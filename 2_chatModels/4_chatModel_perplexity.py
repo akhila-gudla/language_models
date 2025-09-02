f36000b6174d4d315eff67beba1d83f0bb59c918
Akhila Gudla <akhila.gudla@tsgforce.com>
2025-09-02 14:39:00 +0530

    added comment

--- 2_chatModels/4_chatModel_perplexity.py
+++ 2_chatModels/4_chatModel_perplexity.py
@@ -2,6 +2,7 @@ from langchain_perplexity import ChatPerplexity
 from dotenv import load_dotenv
 
 load_dotenv()
+# https://docs.perplexity.ai/getting-started/models
 model = ChatPerplexity( model="sonar")
 result =model.invoke("what is capital of india")
 print(result.content)
