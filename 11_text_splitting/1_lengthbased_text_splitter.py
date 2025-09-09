7c45b132269adb397c92c4f18538d3a3cbe81125
Akhila Gudla <akhila.gudla@tsgforce.com>
2025-09-09 16:00:33 +0530

    update

---
+++ 11_text_splitting/1_lengthbased_text_splitter.py
@@ -0,0 +1,17 @@
+# simply based on length it will chunk the text
+from langchain.text_splitter import CharacterTextSplitter
+from langchain_community.document_loaders import PyPDFLoader
+text = """These missions have not only expanded our knowledge of the universe but have also
+contributed to advancements in technology here on Earth. Satellite communications, GPS, and
+even certain medical imaging techniques trace their roots back to innovations driven by
+space programs."""
+
+loader = PyPDFLoader("/Users/gudlaakhila/Desktop/language_models/10_loaders/computer.pdf")
+documents = loader.load()
+
+splitter = CharacterTextSplitter(
+    separator='', 
+    chunk_size=100, 
+    chunk_overlap=0)
+chunks = splitter.split_documents(documents)
+print(chunks)
